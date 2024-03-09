b3cd586f17338268325d56574d38acef9042216f
thewunmiolabisi <Motlaw2020@gmail.com>
2024-03-09 23:38:17 +0100

    my frist commit

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+// import logo from '../logo.svg';
+import './App.css';
+import LandingPageHeader from './components/LandingPageHeader';
+export function App() {
+  return (
+    <div>
+   <LandingPageHeader/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/app/components/logo.jsx
@@ -0,0 +1,9 @@
+import SpotifyWhite from '../assets/images/SpotifyWhite.png';
+
+export function Logo() {
+  return (
+    <a href='#/dashboard'>
+    <img src={SpotifyWhite} alt="Spotify logo" />
+    </a>
+  );
+}
